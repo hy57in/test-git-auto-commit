823c76c70765abf0c2eea476c75d4df4788b57b2
HyojinKim <gywls00100@gmail.com>
2021-06-08 08:12:33 +0900

    Auto Commit: tracking only

--- file/baseProgram.py
+++ file/baseProgram.py
@@ -53,9 +53,9 @@ while choice != 6:
         subprocess.call(['sh', './continue.sh'])
         subprocess.call(['sh', './autoCommitProcess.sh'])
 
-    elif choice == 3:
-        file_name = str(input('Specify file name to detect : '))
-        subprocess.call(['sh', './file_autoCommitProcess.sh', file_name])
+    # elif choice == 3:
+    #     file_name = str(input('Specify file name to detect : '))
+    #     subprocess.call(['sh', './file_autoCommitProcess.sh', file_name])
         
     elif choice == 3:
         subprocess.call(['bash', './killProcess.sh'])
