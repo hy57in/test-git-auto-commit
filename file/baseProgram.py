88e9e97a5cddf90a7de71f1a682ce41b9917e10e
HyojinKim <gywls00100@gmail.com>
2021-06-08 07:59:29 +0900

    Auto Commit: tracking only

--- file/baseProgram.py
+++ file/baseProgram.py
@@ -12,20 +12,37 @@ def specify_filename():
     file_name = str(input('Specify file name to detect : '))
     subprocess.call(['sh', './file_commit.sh', file_name])
 
+# 특정 파일의 특정 구간 지정
+def specify_filesection():
+    specify_filename()
+    
+    file_section = str(input('Do you want to specify section? [y/n] '))
+
+    if file_section == "y" or file_section == "Y":
+        file_section_start = str(input('Specify start section number in file to detect : '))
+        file_section_finish = str(input('Specify finish section number in file to detect : '))
+        subprocess.call(['sh', './file_section_commit.sh', file_section_start, file_section_finish])
+    else:
+        subprocess.call(['sh', './file_commit.sh', file_name])
+
+def specify_percent():
+    specify_filename() # 특정 파일 지정
+    
+    percent = str(input('What percenttage change do you want to commit?'))
+
+    subprocess.call(['sh', './file_npercent_change.sh', percent])
 
 choice = 0
 
-while choice != 9:
+while choice != 6:
     print("Menu")
     print("1. New")
     print("2. Continue")
-    print("3. Specify file name mode")
-    print("4. Specify file section mode")
-    print("5. Specify percent of all files mode")
-    print("6. Git add file")
-    print("7. Git push to branch")
-    print("8. Delete auto-commit branch")
-    print("9. Exit")
+    print("3. Select mode")
+    print("4. Git add file")
+    print("5. Git push to branch")
+    print("6. Delete auto-commit branch")
+    print("7. Exit")
     choice = int(input(">> "))
 
     if choice == 1:
@@ -38,9 +55,9 @@ while choice != 9:
 
     elif choice == 3:
         file_name = str(input('Specify file name to detect : '))
-        #subprocess.call(['sh', './fileNameProcess.sh', file_name])
+        subprocess.call(['sh', './file_autoCommitProcess.sh', file_name])
         
-    elif choice == 4:
+    elif choice == 3:
         path = "./code/"
         file_list = os.listdir(path)
 
@@ -61,10 +78,7 @@ while choice != 9:
                 subprocess.call(['sh', './continue.sh'])
                 subprocess.call(['sh', './autoCommitProcess.sh'])
 
-    elif choice == 5:
-        pass
-
-    elif choice == 6:
+    elif choice == 4:
         subprocess.call(['bash', './killProcess.sh'])
 
         filename = str(input("What file to add?(file_name) "))
@@ -73,7 +87,7 @@ while choice != 9:
         subprocess.call(['sh', './continue.sh'])
         subprocess.call(['sh', './autoCommitProcess.sh'])
 
-    elif choice == 7:
+    elif choice == 5:
         subprocess.call(['bash', './killProcess.sh'])
         branch = str(input("Where to push?(branch_name) "))
         msg = str(input("Write commit message: "))
@@ -83,14 +97,14 @@ while choice != 9:
         subprocess.call(['sh', './continue.sh'])
         subprocess.call(['sh', './autoCommitProcess.sh'])
 
-    elif choice == 8:
+    elif choice == 6:
         subprocess.call(['bash', './killProcess.sh'])
 
         branch = str(input("Where to checkout?(branch_name) "))
 
         subprocess.call(['bash', './deleteBranch.sh', branch])
 
-    elif choice == 9:
+    elif choice == 7:
         subprocess.call(['bash', './killProcess.sh'])
         break
 
