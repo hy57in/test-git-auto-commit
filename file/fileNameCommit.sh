f8893ce14fe8ec125dc8a41c2e0cc032ab53a1c7
HyojinKim <gywls00100@gmail.com>
2021-06-07 23:24:33 +0900

    Auto Commit

--- file/fileNameCommit.sh
+++ file/fileNameCommit.sh
@@ -10,9 +10,7 @@ do
     git config --global credential.helper store
     git config --global credential.helpter cache
 
-    git --no-pager
-
-    git checkout -b $file_name
+    git --no-pager checkout -b $file_name
 
     #git --no-pager checkout auto-commit
     git --no-pager add $file_name
