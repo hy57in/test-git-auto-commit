7d1a18bcddf54f98d3f47ad74e69e1128649606f
HyojinKim <gywls00100@gmail.com>
2021-06-07 23:18:31 +0900

    Auto Commit

--- file/fileNameCommit.sh
+++ file/fileNameCommit.sh
@@ -12,9 +12,9 @@ do
 
     git --no-pager
 
-    git checkout -b auto-commit
-    
-    git --no-pager checkout auto-commit
+    git checkout -b $file_name
+
+    #git --no-pager checkout auto-commit
     git --no-pager add $file_name
     git --no-pager commit -m "Auto Commit: tracking only $file_name"
     git --no-pager push -u origin auto-commit
