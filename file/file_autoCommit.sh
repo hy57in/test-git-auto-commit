528ae7fa29a23b00166e41139658feef73f70d00
HyojinKim <gywls00100@gmail.com>
2021-06-08 10:36:43 +0900

    Auto Commit: tracking only

--- file/file_autoCommit.sh
+++ file/file_autoCommit.sh
@@ -2,17 +2,17 @@
 while :
 do
 
-  specify_filename="$1"
+  specify_filename_="$1"
   echo "Specify file name"
 
   git config --global credential.helper store
   git config --global credential.helpter cache
 
-  if ! git diff $specify_filename --quiet
+  if ! git diff $specify_filename_ --quiet
   then
     #git --no-pager checkout $specify_filename
-    git --no-pager add $specify_filename
-    git --no-pager commit -a -m "Auto Commit: tracking only $specify_filename"
+    git --no-pager add $specify_filename_
+    git --no-pager commit -a -m "Auto Commit: tracking only $specify_filename_"
     git --no-pager push -u origin auto-commit
 
   # else
