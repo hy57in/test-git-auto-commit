dd1592764108cc92d37fa7bc4abbef63e3078f70
HyojinKim <gywls00100@gmail.com>
2021-06-08 16:19:28 +0900

    Auto Commit: tracking only

--- file/file_npercent.sh
+++ file/file_npercent.sh
@@ -4,13 +4,14 @@ do
   echo "Checking file change for n%"
   filename="$1"
   n="$2"
+  ONE_HUNDRED=
   diff_msg=`git diff --stat $filename`
 
   change_line=$(echo $diff_msg | cut -f  3 -d' ')
 
   FILE_ROW_COUNT=$(cat $filename| wc -l)
   change=`expr $change_line / $FILE_ROW_COUNT`
-  change_percent
+  change_percent=`expr $change \* $`
   echo "$FILE_ROW_COUNT"
 
   FILE_ROW_COUNT=$(cat `./b.py`| wc -l)
