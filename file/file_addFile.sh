83900cd52e494ddf93b9b0a57222cd423dde4097
HyojinKim <gywls00100@gmail.com>
2021-06-08 12:59:51 +0900

    Auto Commit: tracking only

--- file/file_addFile.sh
+++ file/file_addFile.sh
@@ -4,9 +4,9 @@ echo "git add file"
 if [ "$1" = "" ]
 then echo "please write file name which need to git add"
 else
-    specify_filename="$1"
-    #git checkout -b specify_file
-    git add $specify_filename
-    git commit -m "Add file::$specify_filename"
-    git push -u origin auto-commit
+  specify_filename="$1"
+#git checkout -b specify_file
+  git add $specify_filename
+  git commit -m "Add file::$specify_filename"
+  git push -u origin auto-commit
 fi
