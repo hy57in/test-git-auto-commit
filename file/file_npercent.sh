b8dd6e69f421974cb03540f17a3a987bcf6c1afb
HyojinKim <gywls00100@gmail.com>
2021-06-08 15:10:04 +0900

    Auto Commit: tracking only

--- file/file_npercent.sh
+++ file/file_npercent.sh
@@ -9,3 +9,24 @@ git add $file_name
 git diff $file_name
 
 # n%만 변경 감지 어떻게?
+
+#!/bin/bash
+while :
+do
+  # echo "Specify file name"
+  specify_filename="$1"
+  echo "Checking file change for n%"
+
+  if ! git diff --quiet $specify_filename 
+  then
+    git checkout auto-commit
+    git add $specify_filename
+    git commit -m "Auto Commit: tracking $specify_filename"
+    git push -u origin auto-commit
+
+  # else
+    #echo "Working tree clean. Nothing to commmit."
+  fi
+
+  sleep 60
+done
