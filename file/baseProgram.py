d89049452ed64b64f43d0ca8c7a553b9c30f391c
HyojinKim <gywls00100@gmail.com>
2021-06-08 10:22:33 +0900

    fiile

--- file/baseProgram.py
+++ file/baseProgram.py
@@ -58,6 +58,7 @@ while choice != 6:
 
         specify_filename = str(input("Specify file_name to detect : "))
 
+        subprocess.call(['sh', './file_addFile.sh', specify_filename])
         subprocess.call(['sh', './file_autoCommit.sh', specify_filename])
 
     elif choice == 4:
