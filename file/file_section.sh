a3d5b416f781c91eb065992aa8921cff535151e5
HyojinKim <gywls00100@gmail.com>
2021-06-09 11:14:30 +0900

    Auto Commit: tracking only

--- file/file_section.sh
+++ file/file_section.sh
@@ -5,19 +5,16 @@ do
 
   filename="$1"
   n="$2"
-  m="$"
+  m="$3"
   diff_msg=`git diff --stat $filename`
 
   FILE_ROW_COUNT=$(cat $filename| wc -l) # 전체 줄 수 
-  change=`expr $change_line / $FILE_ROW_COUNT` # 변경된 줄 수 / 전체 줄 수 
-  change_percent=`expr $change \* $_100` # percent = 변경된 줄 수 / 전체 줄 수 * 100
-  echo "$FILE_ROW_COUNT"
 
   if ! git diff --quiet && $change_percent > $n
   then
     git checkout auto-commit
     git add $filename
-    git commit -m "Auto Commit: More than $n percent change detected."
+    git commit -m "Auto Commit: $n ~ $m change detected."
     git push -u origin auto-commit
 
   # else
