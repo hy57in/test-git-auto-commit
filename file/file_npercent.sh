d6e94ad1c96aadd301ea31ab04da415f42d0d00e
HyojinKim <gywls00100@gmail.com>
2021-06-19 15:02:23 +0900

    d

--- file/file_npercent.sh
+++ file/file_npercent.sh
@@ -10,7 +10,7 @@ do
   diff_msg=`git diff --stat $filename`
 
   FILE_ROW_COUNT=$(cat $filename| wc -l) # 전체 줄 수 
-  change_line=$(echo $diff_msg | cut -f  3 -d' ') # 변경된 줄 수 
+  change_line=$(echo $diff_msg | cut -f 3 -d' ') # 변경된 줄 수 
   change=`expr $change_line / $FILE_ROW_COUNT` # 변경된 줄 수 / 전체 줄 수 
   change_percent=`expr $change \* $_100` # percent = 변경된 줄 수 / 전체 줄 수 * 100
   echo "전체 줄 수 : $FILE_ROW_COUNT"
