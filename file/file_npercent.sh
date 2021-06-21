34fcf773807c9f77b765e0c4ac67072fd07585a8
HyojinKim <gywls00100@gmail.com>
2021-06-21 11:22:36 +0900

    Auto Commit: tracking only

--- file/file_npercent.sh
+++ file/file_npercent.sh
@@ -7,11 +7,11 @@ do
   echo "Checking file change for $n%"
 
   diff_msg=`git diff --stat $filename`
-  total_changes=$(cat $filename| wc -m) # 전체 문자 수 
-  change_line=$(echo $diff_msg | cut -f 3 -d' ') # 변경된 줄 수 
-  change=`expr $change_line \* 100` # 변경된 줄 수 / 전체 줄 수 
-  change_percent=`expr $change / $FILE_ROW_COUNT` # percent = 변경된 줄 수 / 전체 줄 수 * 100
-  echo "전체 문자 수 : $FILE_ROW_COUNT"
+  total_char=$(cat $filename| wc -m) # 전체 문자 수 
+  changes_char=$(echo $diff_msg | cut -f 3 -d' ') # 변경된 줄 수 
+  changes=`expr $change_line \* 100` # 변경된 줄 수 / 전체 줄 수 
+  change_percent=`expr $change / $total_char` # percent = 변경된 줄 수 / 전체 줄 수 * 100
+  echo "전체 문자 수 : $total_char"
   echo "변경된 문자 수 : $change_line"
   echo "변경 : $change"
   echo "변경된 퍼센트 : $change_percent"
